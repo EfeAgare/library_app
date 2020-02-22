12cb30dfa1245fed2f1ccd5924f00da0d8422ef1
Efe <knowledgeagare157@gmail.com>
2020-02-22 15:54:23 +0100

    undersatnding the use of computed and observers

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,25 @@
+/* eslint-disable ember/no-observers */
+/* eslint-disable no-console */
 import Controller from '@ember/controller';
+import { computed } from "@ember/object";
 
 export default Controller.extend({
 
-  isDisabled: true
+  emailAddress: "",
+  responseMessage: "",
+  headerMessage: "Coming Soon",
+
+  isValid: computed.match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: computed.not('isValid'),
+  
+  actions: {
+    saveInvitation() {
+      alert(`Saving of ${this.get('emailAddress')} is in progress`);
+      this.set('responseMessage', `Thank you!, We've just saved your email address: ${this.get('emailAddress')}`);
+      this.set('headerMessage', `Welcome ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+      
+    }
+  }
+
 })
